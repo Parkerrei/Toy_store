3f09dd9d4f837d2026c67bfd27a5214150cf1cbd
peter <peterrei721@gmail.com>
2026-06-10 00:24:52 +0530

    Remove unique constraint from product name field,generated migration and populated product and category model with custom django command tool using BaseCommand

--- bon_app/management/commands/populate_toys.py
+++ bon_app/management/commands/populate_toys.py
@@ -4,7 +4,8 @@ from django.core.management.base import BaseCommand
 from django.core.files import File 
 from django.utils.text import slugify 
 from bon_app.models import Category,Product
-
+import uuid
+from django.db import transaction
 class Command(BaseCommand):
     help = 'Populates the database with toy products' 
     
@@ -19,41 +20,50 @@ class Command(BaseCommand):
             return 
         
         #loop through category folders 
-        for category_name in os.listdir(source_folder):
-            category_path = os.path.join(source_folder,category_name)
+        try:
+            with transaction.atomic():
+                self.stdout.write("starting atomic database population...")
+                for category_name in os.listdir(source_folder):
+                    category_path = os.path.join(source_folder,category_name)
 
-            if os.path.isdir(category_path):
-                category,created = Category.objects.get_or_create(
-                    name=category_name,
-                    defaults={'description':f'all types of {category_name} '}
-                )
+                    if os.path.isdir(category_path):
+                        category,created = Category.objects.get_or_create(
+                            name = category_name,
+                            defaults = {'description':f'all types of {category_name} '}
+                        )
 
-                if created:
-                    self.stdout.write(self.style.SUCCESS(f'created category:{category_name}'))
+                        if created:
+                            self.stdout.write(self.style.SUCCESS(f'created category:{category_name}'))
 
-                #loop through images inside  the category folder 
-                for filename in os.listdir(category_path):
-                    if filename.lower().endswith(('.jpg','.jpeg','.png','webp')):
-                        product_name = os.path.splitext(filename)[0]
-                        file_path = os.path.join(category_path,filename) 
+                        #loop through images inside  the category folder 
+                        for filename in os.listdir(category_path):
+                            if filename.lower().endswith(('.jpg','.jpeg','.png','webp')):
+                                product_name = os.path.splitext(filename)[0]
+                                file_path = os.path.join(category_path,filename) 
 
-                        #check if product already exists to avoid duplicates 
-                        if Product.objects.filter(name=product_name).exists():
-                            self.stdout.write(self.style.WARNING(f'Product {product_name}already exists'))
-                            continue
+                                #check if product already exists to avoid duplicates 
+                                if Product.objects.filter(name=product_name,category=category).exists():
+                                    self.stdout.write(self.style.WARNING(f'Product {product_name}already exists in {category_name}'))
+                                    continue
+        
+                                with open(file_path,'rb') as f:
+                                    django_file = File(f) 
+                                    ext = os.path.splitext(filename)[1]
+                                    unique_filename = f"{uuid.uuid4()}{ext}"
+                                    product = Product(
+                                        category=category,
+                                        name=product_name,
+                                        stock=random.randint(5,50),
+                                        price=random.choice([19.99,29.99,49.99])
+                                    )
 
-                        with open(file_path,'rb') as f:
-                            django_file = File(f) 
-                            product = Product(
-                                category=category,
-                                name=product_name,
-                                stock=random.randint(5,50),
-                                price=random.choice([19.99,29.99,49.99])
+                                    #this saves the model and copies the image to your media folder automatically
+                                    product.image.save(filename,django_file,save=True)
+                                self.stdout.write(self.style.SUCCESS(f'successfully added product:{product_name}'))
+                self.stdout.write(self.style.SUCCESS('\n data population completed!'
+                        ))
+                
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f'\nTransaction roled back due to error:{e}'))
+                
 
-                            )
-
-                            #this saves the model and copies the image to your media folder automatically
-                            product.image.save(filename,django_file,save=True)
-                        self.stdout.write(self.style.SUCCESS(f'successfully added product:{product_name}'))
-        self.stdout.write(self.style.SUCCESS('\n data population completed!'
-                ))
--- bon_app/models.py
+++ bon_app/models.py
@@ -27,7 +27,7 @@ class Category(models.Model):
 class Product(models.Model):
     image = models.ImageField(upload_to='media/')
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10,decimal_places=2)
 
