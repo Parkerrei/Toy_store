f09e2d6c7998d30558236cabe4a16d17b153aecb
unknown <peterrei721@gmail.com>
2026-08-04 18:48:49 +0530

    wiped product database and re run the custom command to populate product images via cli

--- bon_bon_project/settings.py
+++ bon_bon_project/settings.py
@@ -82,7 +82,9 @@ WSGI_APPLICATION = 'bon_bon_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
        default=config('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=0,
+        ssl_require=True # Forces SSL connection securely
+        
 )
 }    
 
--- bon_bon_project/urls.py
+++ bon_bon_project/urls.py
@@ -26,5 +26,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('bon_app.urls')),
-]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
