b7a7f7ea7709f7518191e93e4903dac5becdb604
unknown <peterrei721@gmail.com>
2026-08-11 16:08:36 +0530

    removed dotenv configuration script

--- bon_bon_project/asgi.py
+++ bon_bon_project/asgi.py
@@ -11,11 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv # 👈 Add this
-load_dotenv() # 👈 Add this
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YourProject.settings')
-application = get_wsgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bon_bon_project.settings')
 
