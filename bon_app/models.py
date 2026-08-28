c3d0e682ec8576790e9d6bf0a9a5b93822191a31
unknown <peterrei721@gmail.com>
2026-08-28 18:57:21 +0530

    added unique constraint for cart item so no duplicate rows is created while preventing race condition bug

--- bon_app/models.py
+++ bon_app/models.py
@@ -45,7 +45,10 @@ class CartItem(models.Model):
     product   = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity  = models.PositiveBigIntegerField(default=1)
     added_at  = models.DateTimeField(auto_now_add=True)
-  
+
+    class Meta:
+        unique_together = ('user_cart','product')
+        
     def __str__(self):
         return f"{self.quantity} x {self.product.name} id:{self.id}"
 
