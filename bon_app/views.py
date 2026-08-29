48c4e84dfb98b08af06ae8d22e04fa3d3fd4ca6e
unknown <peterrei721@gmail.com>
2026-08-29 15:48:43 +0530

    deleted incrementing part from add to cart view because we are already incrementing using update_or_create() so the later was redundant as it would have incremented the stock twice

--- bon_app/views.py
+++ bon_app/views.py
@@ -228,12 +228,7 @@ def add_to_cart(request, id):
         defaults={'quantity':models.F('quantity') + 1}
     )
 
-    # 4. If it already exists, increment the quantity
-    if not created:
-        cart_item.quantity = F('quantity') + 1
-        cart_item.save(update_fields=['quantity'])
-        cart_item.refresh_from_db()
-
+    
     # 5. Deduct exactly ONE from stock 
     toy.stock = F('stock') - 1
     toy.save(update_fields=['stock'])
