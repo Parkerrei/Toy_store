dc9b21dc3e69f184aff467d63555db03452b97fc
unknown <peterrei721@gmail.com>
2026-08-29 19:25:43 +0530

    removed extra spacing between views

--- bon_app/views.py
+++ bon_app/views.py
@@ -231,8 +231,6 @@ def add_to_cart(request, id):
         cart_item.save(update_fields=['quantity'])
         cart_item.refresh_from_db()
     
-   
-
     # 5. Deduct exactly ONE from stock 
     toy.stock = F('stock') - 1
     toy.save(update_fields=['stock'])
