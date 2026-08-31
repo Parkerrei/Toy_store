1f4f73311b31a469308401710bf3b86d9ba5616e
unknown <peterrei721@gmail.com>
2026-08-31 15:52:08 +0530

    change signal.py code to overwrite postgress default sequence counter mechanism for every model table.(meaning restart the id to 1 everytime a row is deleted or whole table is deleted,which postgress doesnot do by default

--- bon_app/signals.py
+++ bon_app/signals.py
@@ -5,12 +5,35 @@ from .models import CartItem
 from django.apps import apps
 
 @receiver(post_delete,sender=CartItem)
-def reset_cart_sequence(sender,**kwargs):
+def force_renumber(sender):
     #this sql checks the max id  and sets the next sequence value
     # if the table is empty it resets back to 1
-    
-    with connection.cursor() as cursor:
-        cursor.execute(
-            "SELECT setval(pg_get_serial_sequence('bon_app_CartItem','id' ),"
-            "COALESCE(max(id) , 0) + 1 , false) FROM bon_app_CartItem;"
-        )
+    table = sender._meta.db_table
+    seq = f"{table}_id_seq"
+    with transaction.atomic():
+        with connection.cursor() as cursor:
+                cursor.execute(f'SELECT id FROM "{table} ORDER BY id')
+                old_ids = [r[0] for r in cursor.fetchall()]
+
+                if not old_ids:
+                     cursor.execute(f'ALTER SEQUENCE"{seq}RESTART WITH 1;')
+                     return
+                #avoid clash 
+                cursor.execute(f'UPDATE "{table}"SET id = -id')
+
+                for new_id , old_id in enumerate(old_ids , start = 1):
+                     cursor.execute(
+                          f'UPDATE "{table}"SET id = %s WHERE id = -%s',
+                          [new_id , old_id]
+                     )
+                cursor.execute(f'ALTER SEQUENCE "{seq}" RESTART WITH {len(old_ids) + 1};')
+
+    #Auto-connect to all your app models
+    #change 'toys' to your app name 
+    app_models = apps.get_app_config('bon_app').get_models()
+    for model in app_models:
+         post_delete.connect(
+              lambda sender , **kwargs:force_renumber(sender),
+              sender = model,
+              weak = False
+         )
