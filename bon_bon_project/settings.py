bf44974cb9e5f5a2bb191e76cb1ea670b5fcec9f
Parkerrei <peterrei721@gmail.com>
2026-09-17 17:28:23 +0530

    deleted webservice configuration that was meant for testing purpose

--- bon_bon_project/settings.py
+++ bon_bon_project/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
  
 LOGIN_URL = '/logged'
 MEDIA_URL = '/media/'
