99d96990d749b35f198691aad035939d69a9d784
Parkerrei <peterrei721@gmail.com>
2026-09-17 17:11:35 +0530

    added whitenoise middleware and whitenoise storage variable

--- bon_bon_project/settings.py
+++ bon_bon_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
  
 LOGIN_URL = '/logged'
 MEDIA_URL = '/media/'
