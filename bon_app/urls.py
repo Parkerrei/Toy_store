e07ff37e309d50f48c30e0b5f70d1a6a050713c8
unknown <peterrei721@gmail.com>
2026-09-22 17:41:06 +0530

    removed views simplicity and readability of urls

--- bon_app/urls.py
+++ bon_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-import bon_app.views as views
-from .views import user,user_log_in,main,buy,log_out,add_to_cart,user_cart_items,cart_deduct,user_cart_order_payment,increment_item,decrement_item
+
+from .views import user,user_log_in,main,buy,log_out,add_to_cart,user_cart_items,cart_deduct,user_cart_order_payment,increment_item,decrement_item,category_products_view
 
 urlpatterns = [
                 path('',user,name='user'),
@@ -9,7 +9,7 @@ urlpatterns = [
                 path('buy/<int:productId>/',buy,name='buy'),
                 path('log_out/',log_out,name='log_out'),
                 # This single line handles category 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, etc.
-                path('category_products_view/<slug:slug>/', views.category_products_view, name='category_products_view'),
+                path('category_products_view/<slug:slug>/',category_products_view, name='category_products_view'),
                 path('add_to_cart/<int:productId>/',add_to_cart,name='add_to_cart'),
                 path('user_cart_items/',user_cart_items,name='user_cart_items'),
                 path('cart_deduct/',cart_deduct,name='cart_deduct'),
